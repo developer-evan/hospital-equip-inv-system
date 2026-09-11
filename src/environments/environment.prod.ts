ef41217ee24de2a9525c24147e199ac9b21ccfb4
Mogeni <evansmogeni254@gmail.com>
2026-09-12 01:16:24 +0300

    Update production environment configuration for API URL
    - Commented out the local API URL and replaced it with the production URL for the hospital equipment inventory server, ensuring the application connects to the correct backend service in a production environment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: 'http://localhost:3000',
+  production: true,   
+  // apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://hospital-equipment-inventory-server.onrender.com',
 };
